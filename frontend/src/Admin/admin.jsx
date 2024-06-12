fc6ac7ee64160e44bb379027406d0193d3796b98
Honeyaps <honeyaps12345@gmail.com>
2024-06-12 23:20:31 +0530

    changes

--- frontend/src/Admin/admin.jsx
+++ frontend/src/Admin/admin.jsx
@@ -130,6 +130,7 @@ export default function Admin() {
                   <label>Product Picture</label>
                   <input
                     type="file"
+                    name="filename"
                     onChange={handleFileChange}
                     required
                   />
