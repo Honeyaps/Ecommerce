a595033e807242352e724b47a131de078c7e31ec
Hemant Singh <honeyaps12345@gmail.com>
2024-09-17 18:51:10 +0530

    deploy versel

--- frontend/src/Registration/axiosConfig.jsx
+++ frontend/src/Registration/axiosConfig.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-// const axiosInstance = axios.create({
-//   baseURL: "https://bluorng-backend.vercel.app/v1",
-// });
+const axiosInstance = axios.create({
+  baseURL: "https://bluorng-backend.vercel.app/v1",
+});
 
 // local
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:4900/v1"
-});
+// const axiosInstance = axios.create({
+//   baseURL: "http://localhost:4900/v1"
+// });
 
 export default axiosInstance;
